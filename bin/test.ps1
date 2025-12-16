bbd97007fed74b0815d47e93651407af994e4b7b
ivaquero <msaintms@outlook.com>
2025-12-16 10:01:54 +0800

    update bin

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,6 +1,15 @@
 #Requires -Version 5.1
-#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.7.1' }
+#Requires -Modules @{ ModuleName = 'BuildHelpers'; ModuleVersion = '2.0.1' }
+#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.2.0' }
 
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$result = Invoke-Pester "$PSScriptRoot\..\test" -PassThru
+$pesterConfig = New-PesterConfiguration -Hashtable @{
+    Run    = @{
+        Path     = "$PSScriptRoot/.."
+        PassThru = $true
+    }
+    Output = @{
+        Verbosity = 'Detailed'
+    }
+}
+$result = Invoke-Pester -Configuration $pesterConfig
 exit $result.FailedCount
